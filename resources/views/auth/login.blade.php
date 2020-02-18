f912b95aa1584741bde2e60d8e517592de0dfa80
Tais Hryssai <thryssai@gmail.com>
2020-02-18 16:29:41 -0300

    session

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,30 @@
-@extends('layouts.app')
-
+@extends('layouts.session')
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
+<form class="card" action="{{route('login')}}" method="POST">
+    @csrf
+    <div class="card-body p-6">
+      <div class="card-title">{{ __('Login to your account')}}</div>
+      <div class="form-group">
+        <label class="form-label">{{ __('E-Mail Address')}}</label>
+        <input type="email" class="form-control" id="exampleInputEmail1" name="email" aria-describedby="emailHelp" placeholder="Email">
     </div>
+    <div class="form-group">
+        <label class="form-label">{{ __('Password')}}</label>
+        <input type="password" class="form-control" id="exampleInputPassword1"name="password" placeholder="Password">
+    </div>
+    <div class="form-group">
+        <label class="custom-control custom-checkbox">
+          <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+          <span class="form-check-label remember" for="remember">
+            {{ __('Remember Me')}}
+        </span>
+        
+    </label>
 </div>
+<div class="form-footer">
+    <button type="submit" class="btn btn-primary btn-block">{{ __('Login')}}</button>
+</div>
+<a href="{{ route('password.request')}}" class="float-right senha">{{ __('I forgot password')}}</a>
+</div>
+</form>
 @endsection
---
+++ resources/views/layouts/session.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- CSRF Token -->
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+  <title>{{ config('app.name', 'Laravel') }}</title>
+
+
+  <!-- Scripts -->
+  <script src="{{ asset('js/app.js') }}" defer></script>
+  <!-- Fonts -->
+  <link rel="dns-prefetch" href="//fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+  <!-- Styles -->
+  <link href="{{ asset('css/login/session.css') }}" rel="stylesheet">
+</head>
+<body class="">
+  <div class="page">
+   <div class="page-single">
+    <div class="container py-4">
+      <div class="row">
+        <div class="col col-login mx-auto">
+          <div class="text-center mb-1">
+           <img src="{{ asset('images/logo.png') }}" class="h-7" alt="">
+         </div>
+         <main class="">
+          @yield('content')
+        </main>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</body>
+</html>
