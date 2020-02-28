fdff21d08069f219e2ec5cdb4146605d757a3aaf
Dudalara <eduardawaldemar@hotmail.com>
2020-02-28 13:27:59 -0300

    Organization layouts and flash messages

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,25 +3,25 @@
 
 @section('content')
               
- 
-            <form class="card" method="POST" action="{{ route('password.email') }}">
-            @csrf                                
-                 <div class="card-body p-6">
-                    <div class="card-title">{{ __('Reset Password') }}</div>
-                        <p class="text-muted">{{ __('Enter your email address and your password will be reset and emailed to you.') }}</p>
-                         <div class="form-group">
-                            <label class="form-label" for="email">{{ __('E-Mail Address') }}</label>
+<form class="card" method="POST" action="{{ route('password.email') }}">
+  @csrf                                
+    <div class="card-body p-6">
+        <div class="card-title">
+          {{ __('Reset Password') }}
+        </div>
+        <p class="text-muted">{{ __('Enter your email address and your password will be reset and emailed to you.') }}</p>
+        <div class="form-group">
+            <label class="form-label" for="email">{{ __('E-Mail Address') }}</label>
+            <input id = "email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email">
 
-                           <input id = "email" type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="Email">
-
-                             </div>
-                  <div class="form-footer">
-                  <button type="submit" class="btn btn-primary btn-block">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                   </div>
-                   </div>
-               </form>
-               <div class="text-center text-muted">{{ __('Forget it,')}} <a href="{{ route('login') }}">{{ __('send me back') }}</a> {{ __('to the sign in screen.')}} </div>
+        </div>
+        <div class="form-footer">
+            <button type="submit" class="btn btn-primary btn-block">
+               {{ __('Send Password Reset Link') }}
+            </button>
+        </div>
+    </div>
+</form>
+<div class="text-center text-muted">{{ __('Forget it,')}} <a href="{{ route('login') }}">{{ __('send me back') }}</a> {{ __('to the sign in screen.')}} </div>
          
 @endsection
