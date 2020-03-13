b732c2afda260c948776c980165a214f4da44402
Tais Hryssai <thryssai@gmail.com>
2020-03-13 16:45:28 -0300

    creation of breadcrumbs

--- resources/views/layouts/admin/_breadcrumbs.blade.php
+++ resources/views/layouts/admin/_breadcrumbs.blade.php
@@ -1,5 +1,1 @@
-<nav aria-label="breadcrumb">
-  <ol class="breadcrumb">
-    <li class="breadcrumb-item active" aria-current="page">Página inicial</li>
-  </ol>
-</nav>
+{{ Breadcrumbs::render() }}
