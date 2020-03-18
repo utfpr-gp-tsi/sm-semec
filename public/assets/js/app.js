205a4941c0d60111be385af270da4a7240abd00e
Amanda Carolyne <amandaacarolyne@outlook.com>
2020-03-18 13:17:45 -0300

    database creation using fill

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -37106,6 +37106,17 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
+/***/ "./resources/sass/admin/app.scss":
+/*!***************************************!*\
+  !*** ./resources/sass/admin/app.scss ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+// removed by extract-text-webpack-plugin
+
+/***/ }),
+
 /***/ "./resources/sass/app.scss":
 /*!*********************************!*\
   !*** ./resources/sass/app.scss ***!
@@ -37129,15 +37140,16 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /***/ }),
 
 /***/ 0:
-/*!*************************************************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/login/session.scss ***!
-  \*************************************************************************************************/
+/*!*********************************************************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/login/session.scss ./resources/sass/admin/app.scss ***!
+  \*********************************************************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /var/www/resources/js/app.js */"./resources/js/app.js");
 __webpack_require__(/*! /var/www/resources/sass/app.scss */"./resources/sass/app.scss");
-module.exports = __webpack_require__(/*! /var/www/resources/sass/login/session.scss */"./resources/sass/login/session.scss");
+__webpack_require__(/*! /var/www/resources/sass/login/session.scss */"./resources/sass/login/session.scss");
+module.exports = __webpack_require__(/*! /var/www/resources/sass/admin/app.scss */"./resources/sass/admin/app.scss");
 
 
 /***/ })
