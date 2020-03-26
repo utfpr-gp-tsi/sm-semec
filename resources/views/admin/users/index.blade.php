0fb74dc990124507e14c417703f38377a094b0fa
utfpr-gp-tsi <dmarczal@gmail.com>
2020-03-26 05:53:11 -0300

    Use @each instead of @foreach

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -16,17 +16,7 @@
       </tr>
     </thead>
     <tbody>
-      @foreach($users as $key => $user)
-        <tr>
-          <td><a href="{{route('admin.show.user', $user->id)}}">{{ $user->name }}</a></td>
-          <td>{{ $user->email }}</td>
-          <td>{{ $user->created_at }}</td>
-          <td >
-            @component('components.links.edit', ['url' => route('admin.edit.user', $user->id)]) @endcomponent
-            @component('components.links.delete', ['url' => route('admin.destroy.user', $user->id)]) @endcomponent
-          </td>
-        </tr>
-      @endforeach
+      @each('admin.users._user_row', $users, 'user')
     </tbody>
   </table>
 </div>
