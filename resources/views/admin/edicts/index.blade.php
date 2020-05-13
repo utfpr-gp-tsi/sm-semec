18091d2a945f54faad7b05063288f81acbf863c3
Dudalara <eduardawaldemar@hotmail.com>
2020-05-13 15:02:06 -0300

    teste

--- resources/views/admin/edicts/index.blade.php
+++ resources/views/admin/edicts/index.blade.php
@@ -3,5 +3,21 @@
 @section('title', 'Editais')
 @section('content')
 
+@component('components.index.header', ['search_url' => route('admin.search.users'), 'new_url' => route('admin.new.edict')]) @endcomponent
 
+<div class="table-responsive mt-3">
+  <table class="table card-table table-striped table-vcenter table-data">
+    <thead>
+      <tr>
+        <th>Nome</th>
+        <th>Válido até</th>
+        <th>Aberto em</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+    
+    </tbody>
+  </table>
+</div>
 @endsection
