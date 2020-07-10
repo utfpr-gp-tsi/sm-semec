eaf68e724909a67912ec6e409f9995d867a93bc6
Tais Hryssai <thryssai@gmail.com>
2020-07-10 19:48:08 -0300

    servants area, unit tests and browser

--- resources/js/servants/app.js
+++ resources/js/servants/app.js
@@ -1,7 +1,5 @@
 require('../bootstrap');
 require('../../config/js/namespace');
-require('../../lib/img_preview/js/image_preview');
-require('../../lib/search/js/submit_search');
 
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
