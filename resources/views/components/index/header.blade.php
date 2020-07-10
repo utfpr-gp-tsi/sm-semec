28fc1c217bd9a15e2802cf69d0ca35597619b7ef
Dudalara <eduardawaldemar@hotmail.com>
2020-07-10 19:42:34 -0300

    error webserver

--- resources/views/components/index/header.blade.php
+++ resources/views/components/index/header.blade.php
@@ -15,7 +15,7 @@
   </div>
   <div class="col-sm-12 col-lg-4">
     @isset($new_url)
-      <a href="{{ $new_url }}" class="btn btn-outline-primary d-block">
+      <a href="{{ $new_url }}" class="btn btn-outline-primary d-block"> 
         <i class="fas fa-plus"></i>
         {{ $new_btn_name }}
       </a>
